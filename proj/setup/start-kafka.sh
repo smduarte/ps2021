eed372f47f1e794f399cd05f074eeb3cce4ff646
smd <smd@fct.unl.pt>
2021-04-19 01:00:56 +0100

    fixed network name

--- proj/setup/start-kafka.sh
+++ proj/setup/start-kafka.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-[ ! "$(docker network ls | grep ps-net )" ] && \
-	docker network create --driver=bridge --subnet=10.123.0.0/16 ps-net
+[ ! "$(docker network ls | grep psnet )" ] && \
+	docker network create --driver=bridge --subnet=10.123.0.0/16 psnet
 
 docker pull smduarte/ps2021-kafka
 
@@ -11,5 +11,5 @@ docker rm -f kafka
 
 docker run -h kafka  \
 	--name=kafka \
-	--network=ps-net \
+	--network=psnet \
 	--rm -t -p 9092:9092 -p 2181:2181 smduarte/ps2021-kafka
