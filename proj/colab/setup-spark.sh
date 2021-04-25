a18783b6797e4358550fc41ca706023c8f74a9db
smd <smd@fct.unl.pt>
2021-04-25 13:38:37 +0100

    fixed missed extension

--- proj/colab/setup-spark.sh
+++ proj/colab/setup-spark.sh
@@ -16,8 +16,8 @@ REPO=https://github.com/smduarte/ps2021/raw/master/proj/colab/
 JARS_KAFKA010=(spark-sql-kafka-0-10_2.11-$SPARK_VERSION.jar \
                spark-streaming-kafka-0-10-assembly_2.11-$SPARK_VERSION.jar \
                spark-streaming-kafka-0-10_2.11-$SPARK_VERSION.jar \
-               spark-streaming-kafka-0-8-assembly_2.11-$SPARK_VERSION \
-               spark-streaming-kafka-0-8_2.11-$SPARK_VERSION \
+               spark-streaming-kafka-0-8-assembly_2.11-$SPARK_VERSION.jar \
+               spark-streaming-kafka-0-8_2.11-$SPARK_VERSION.jar \ 
                )
 
 for jar in "${JARS_KAFKA010[@]}"
