3d8787ce0ecfd2145485692bcfb324ea92225f53
smd <smd@fct.unl.pt>
2021-04-25 14:22:12 +0100

    added publisher to start script

--- proj/colab/start-kafka.sh
+++ proj/colab/start-kafka.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo $*
 
 KAFKA=kafka_2.12-2.7.0
 
@@ -12,4 +13,4 @@ echo "Starting Kafka..."
 echo "Waiting for 10 secs until kafka and zookeeper services are up and running"
 sleep 10
 
-
+java -cp /tmp/PS2021-Debs-Publisher.jar debs.Publisher $*
