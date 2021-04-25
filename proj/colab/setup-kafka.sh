d68429a2825ff95b2941ea14ceb5205d5ed56676
smd <smd@fct.unl.pt>
2021-04-25 13:50:13 +0100

    added setup-kafka

---
+++ proj/colab/setup-kafka.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Downloading Kafka..."
+wget -q -O  /tmp/kafka.tgz https://downloads.apache.org/kafka/2.7.0/$KAFKA.tgz
+tar -xzf /tmp/kafka.tgz
+
+echo "Starting Zookeeper..."
+./$KAFKA/bin/zookeeper-server-start.sh -daemon ./$KAFKA/config/zookeeper.properties
+echo "Starting Kafka..."
+./$KAFKA/bin/kafka-server-start.sh -daemon ./$KAFKA/config/server.properties
+echo "Waiting for 10 secs until kafka and zookeeper services are up and running"
+sleep 10
+
+
