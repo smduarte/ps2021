b3d6d64067e3c5909b6679fe720f34bc6624c433
smduarte <smd@fct.unl.pt>
2021-04-28 23:28:34 +0100

    Update setup-spark+kafka.sh

--- proj/colab/setup-spark+kafka.sh
+++ proj/colab/setup-spark+kafka.sh
@@ -13,10 +13,16 @@ pip install -q findspark
 REPO=https://github.com/smduarte/ps2021/raw/master/proj/colab
 
 JARS_KAFKA=(spark-sql-kafka-0-10_2.11-$SPARK_VERSION.jar \
-               spark-streaming-kafka-0-10-assembly_2.11-$SPARK_VERSION.jar \
-               spark-streaming-kafka-0-10_2.11-$SPARK_VERSION.jar \
+#               spark-streaming-kafka-0-10-assembly_2.11-$SPARK_VERSION.jar \
+#               spark-streaming-kafka-0-10_2.11-$SPARK_VERSION.jar \
                spark-streaming-kafka-0-8-assembly_2.11-$SPARK_VERSION.jar \
                spark-streaming-kafka-0-8_2.11-$SPARK_VERSION.jar)
+
+#JARS_KAFKA=(spark-sql-kafka-0-10_2.11-$SPARK_VERSION.jar \
+#               spark-streaming-kafka-0-10-assembly_2.11-$SPARK_VERSION.jar \
+#               spark-streaming-kafka-0-10_2.11-$SPARK_VERSION.jar \
+#               spark-streaming-kafka-0-8-assembly_2.11-$SPARK_VERSION.jar \
+#               spark-streaming-kafka-0-8_2.11-$SPARK_VERSION.jar)
 
 for jar in "${JARS_KAFKA[@]}"
 do
