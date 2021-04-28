05cf17e8cceb8e830e9a38a2eb1c8be936c73fe0
smduarte <smd@fct.unl.pt>
2021-04-28 23:53:10 +0100

    Update setup-spark311.sh

--- proj/colab/setup-spark311.sh
+++ proj/colab/setup-spark311.sh
@@ -12,8 +12,8 @@ pip install -q findspark
 
 REPO=https://github.com/smduarte/ps2021/raw/master/proj/colab/
 
-JARS_KAFKA=(spark-streaming-kafka-0-8-assembly_2.11-$SPARK_VERSION.jar \
-               spark-streaming-kafka-0-8_2.11-$SPARK_VERSION.jar)
+JARS_KAFKA=(spark-streaming-kafka-0-8-assembly_2.11-$SPARK_VERSION.jar.gz \
+               spark-streaming-kafka-0-8_2.11-$SPARK_VERSION.jar.gz)
 
 #JARS_KAFKA=(spark-sql-kafka-0-10_2.11-$SPARK_VERSION.jar \
 #               spark-streaming-kafka-0-10-assembly_2.11-$SPARK_VERSION.jar \
